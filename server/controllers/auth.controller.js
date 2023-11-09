94a95006f662ea0780861261e23391318052adfd
Ulvi Spaethe <ulvispaethe@gmail.com>
2023-11-09 12:14:34 +0000

    sign in route & cookie added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,17 +1,51 @@
-const User = require("../models/user.model")
-const bcryptjs = require('bcryptjs');
+const User = require("../models/user.model");
+const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
-const signUp = async (req,res, next) =>{
-    const {username , email, password} = req.body
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+const jwt = require("jsonwebtoken");
+const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({username: username, email: email, password: hashedPassword})
-    try {
-        await newUser.save()
-        res.status(201).json({message: "User created"})
-    } catch (error) {
-        next(errorHandler(300, error.message))
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    next(errorHandler(300, error.message));
+  }
+};
+
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
     }
-}
 
-module.exports = {signUp}
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000) //1 hour
+    res.cookie(
+        "access_token",
+        token,
+        { httpOnly: true },
+        (maxage = 1000 * 60 * 60 * 24 * 7)
+      )
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, error.message));
+  }
+};
+
+module.exports = { signUp, signIn };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { signUp } = require('../controllers/auth.controller.js')
+const { signUp, signIn } = require('../controllers/auth.controller.js')
 
 const router = express.Router()
 
 router.post('/signup', signUp)
+router.post('/signin', signIn)
+
 
 module.exports = router
